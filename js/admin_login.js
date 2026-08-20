1c7a213f2691219ac1c1a720bbf9569195f628c9
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2026-08-20 14:57:53 +0530

    admin.css, login.js

---
+++ js/admin_login.js
@@ -0,0 +1,11 @@
+const loginForm = document.getElementById("login-form");
+const loginMessage = document.getElementById("login-message");
+
+const API_URL = "http://127.0.0.1:8000";
+
+loginForm.addEventListener("submit", async (event) => {
+
+  
+
+
+})
