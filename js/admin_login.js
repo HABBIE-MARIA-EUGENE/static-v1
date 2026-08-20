43652b8d8c0b6d89b5249646610af0451fb933a0
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2026-08-20 15:12:14 +0530

    login-tested

--- js/admin_login.js
+++ js/admin_login.js
@@ -5,7 +5,68 @@ const API_URL = "http://127.0.0.1:8000";
 
 loginForm.addEventListener("submit", async (event) => {
 
-  
+  event.preventDefault();
+
+  const username =
+    document.getElementById("username").value;
+
+  const password =
+    document.getElementById("password").value;
 
 
-})
+  const formData = new URLSearchParams();
+
+  formData.append("username", username);
+  formData.append("password", password);
+
+
+  try {
+
+    const response = await fetch(
+      `${API_URL}/auth/login`,
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type":
+            "application/x-www-form-urlencoded"
+        },
+
+        body: formData
+      }
+    );
+
+
+    const data = await response.json();
+
+
+    if (!response.ok) {
+      throw new Error(
+        data.detail || "Login failed"
+      );
+    }
+
+
+    // Save JWT
+    localStorage.setItem(
+      "access_token",
+      data.access_token
+    );
+
+
+    // Go to dashboard
+    window.location.href = "dashboard.html";
+
+
+  } catch (error) {
+
+    console.error("Login error:", error);
+
+    loginMessage.textContent =
+      error.message || "Unable to login.";
+
+  }
+
+});
+
+
